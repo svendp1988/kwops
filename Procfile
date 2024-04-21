79a71710e4080b9194ad63d08adb7018efb8691e
Sven <sven.depotter@hotmail.com>
2024-04-21 13:00:10 +0200

    trying to deploy multiproject to heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar hr/build/libs/hr-0.0.1-SNAPSHOT.jar
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar hr/build/libs/devops-0.0.1-SNAPSHOT.jar
--- devops/src/main/java/pxl/kwops/devops/DevopsApplication.java
+++ devops/src/main/java/pxl/kwops/devops/DevopsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import pxl.kwops.devops.boundary.event.MessageReceiverImpl;
 
 import java.util.concurrent.TimeUnit;
